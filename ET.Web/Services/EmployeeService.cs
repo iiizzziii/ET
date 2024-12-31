20ec2c27fafdc47d821347f91fed882b98c04d0a
iiizzziii <mokos.jakub@gmail.com>
2024-12-31 18:38:28 +0100

    nav bar updated

--- ET.Web/Services/EmployeeService.cs
+++ ET.Web/Services/EmployeeService.cs
@@ -1,6 +1,5 @@
 using ET.Models;
 using System.Net.Http.Json;
-using System.Text;
 
 namespace ET.Web.Services;
 
@@ -8,56 +7,8 @@ namespace ET.Web.Services;
 
 public class EmployeeService(HttpClient httpClient) : IEmployeeService
 {
-    public async Task<IEnumerable<Employee>> GetEmployees()
-    {
-        try
-        {
-            return await httpClient.GetFromJsonAsync<IEnumerable<Employee>>("api/employees");
-        }
-        catch (Exception e) { Console.WriteLine(e); throw; }
-    }
-
-    public async Task<bool> UpdateEmployee(EmployeeForm employee)
-    {
-        try
-        {
-            // update models
-            var updateEmployee = new EmployeeDto
-            {
-                Name = employee.Name,
-                Surname = employee.Surname,
-                BirthDate = employee.BirthDate,
-                Position = employee.Position,
-                IpAddress = employee.IpAddress,
-            };
-            
-            var response = await httpClient.PutAsJsonAsync($"api/employees/{employee.Id}", updateEmployee);
-
-            return response.IsSuccessStatusCode;
-        }
-        catch (Exception e) { Console.WriteLine(e); throw; }
-    }
+    private const string ApiUri = "api/employees";
     
-    public async Task<bool> DeleteEmployee(int id)
-    {
-        try
-        {
-            var response = await httpClient.DeleteAsync($"api/employees/{id}");
-
-            return response.IsSuccessStatusCode;
-        }
-        catch (Exception e) { Console.WriteLine(e); throw; }
-    }
-
-    public async Task<IEnumerable<string>> GetPositions()
-    {
-        try
-        {
-            return await httpClient.GetFromJsonAsync<IEnumerable<string>>("api/employees/positions");
-        }
-        catch (Exception e) { Console.WriteLine(e); throw; }
-    }
-
     public async Task<bool> AddEmployee(EmployeeForm employee)
     {
         try
@@ -71,18 +22,64 @@ public class EmployeeService(HttpClient httpClient) : IEmployeeService
                 IpAddress = employee.IpAddress,
             };
             
-            var response = await httpClient.PostAsJsonAsync("api/employees/add", newEmployee);
-
+            var response = await httpClient.PostAsJsonAsync($"{ApiUri}/add", newEmployee);
             return response.IsSuccessStatusCode;
         }
         catch (Exception e) { Console.WriteLine(e); throw; }
     }
-
+    
+    public async Task<bool> UpdateEmployee(EmployeeForm employee)
+    {
+        try
+        {
+            var updateEmployee = new EmployeeDto
+            {
+                Name = employee.Name,
+                Surname = employee.Surname,
+                BirthDate = employee.BirthDate,
+                Position = employee.Position,
+                IpAddress = employee.IpAddress,
+            };
+            
+            var response = await httpClient.PutAsJsonAsync($"{ApiUri}/{employee.Id}", updateEmployee);
+            return response.IsSuccessStatusCode;
+        }
+        catch (Exception e) { Console.WriteLine(e); throw; }
+    }
+    
+    public async Task<bool> DeleteEmployee(int id)
+    {
+        try
+        {
+            var response = await httpClient.DeleteAsync($"{ApiUri}/{id}");
+            return response.IsSuccessStatusCode;
+        }
+        catch (Exception e) { Console.WriteLine(e); throw; }
+    }
+    
+    public async Task<IEnumerable<Employee>> GetEmployees()
+    {
+        try
+        {
+            return await httpClient.GetFromJsonAsync<IEnumerable<Employee>>(ApiUri);
+        }
+        catch (Exception e) { Console.WriteLine(e); throw; }
+    }
+    
+    public async Task<IEnumerable<string>> GetPositions()
+    {
+        try
+        {
+            return await httpClient.GetFromJsonAsync<IEnumerable<string>>($"{ApiUri}/positions");
+        }
+        catch (Exception e) { Console.WriteLine(e); throw; }
+    }
+    
     public async Task<HttpResponseMessage> AddEmployeesJson(EmployeesDto json)
     {
         try
         {
-            return await httpClient.PostAsJsonAsync("api/employees/add/json", json);
+            return await httpClient.PostAsJsonAsync($"{ApiUri}/add/json", json);
         }
         catch (Exception e) { Console.WriteLine(e); throw; }
     }
@@ -91,21 +88,8 @@ public class EmployeeService(HttpClient httpClient) : IEmployeeService
     {
         try
         {
-            return await httpClient.PostAsJsonAsync("api/employees/add/json/positions", json);
+            return await httpClient.PostAsJsonAsync($"{ApiUri}/add/json/positions", json);
         }
         catch (Exception e) { Console.WriteLine(e); throw; }
     }
-    
-    // public async Task<EmployeeDto?> GetEmployee(int id)
-    // {
-    //     try
-    //     {
-    //         var response = await httpClient.GetAsync($"api/employees/{id}");
-    //         if (response.IsSuccessStatusCode)
-    //         {
-    //             return await response.Content.ReadFromJsonAsync<EmployeeDto>();
-    //         }
-    //         throw new Exception(await response.Content.ReadAsStringAsync());
-    //     } catch (Exception e) { Console.WriteLine(e); throw; }
-    // }
 }
--- ET.Web/Services/IEmployeeService.cs
+++ ET.Web/Services/IEmployeeService.cs
@@ -4,12 +4,11 @@ namespace ET.Web.Services;
 
 public interface IEmployeeService
 {
-    // Task<EmployeeDto?> GetEmployee(int id);
-    Task<IEnumerable<Employee>> GetEmployees();
+    Task<bool> AddEmployee(EmployeeForm employee);
     Task<bool> UpdateEmployee(EmployeeForm employee);
     Task<bool> DeleteEmployee(int id);
+    Task<IEnumerable<Employee>> GetEmployees();
     Task<IEnumerable<string>> GetPositions();
-    Task<bool> AddEmployee(EmployeeForm employee);
     Task<HttpResponseMessage> AddEmployeesJson(EmployeesDto json);
     Task<HttpResponseMessage> AddPositionsJson(PositionsDto json);
 }
