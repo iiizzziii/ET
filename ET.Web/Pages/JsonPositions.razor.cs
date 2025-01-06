dfb4254b5cd0fdae857457fc8df8bc4378742970
iiizzziii <mokos.jakub@gmail.com>
2025-01-06 11:18:47 +0100

    upload positions updated

--- ET.Web/Pages/JsonPositions.razor.cs
+++ ET.Web/Pages/JsonPositions.razor.cs
@@ -1,11 +1,8 @@
 using ET.Models;
 using System.Net;
 using System.Text.Json;
-using System.Text.Json.Nodes;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.JSInterop;
 
 #pragma warning disable CA1869
 
@@ -13,32 +10,21 @@ namespace ET.Web.Pages;
 
 public partial class JsonPositions : ComponentBase
 {
-    private bool _popupVisible;
-    private string _jsonInput = string.Empty;
-    private string _message = string.Empty;
-    private string _popupMessage = string.Empty;
-    private bool isDragging;
-
     private PositionsDto? _positions;
     private string? _validationMessage;
     private string? _responseContent;
 
-    private async Task HandleFileSelected(InputFileChangeEventArgs e)
+    private async Task HandleFile(InputFileChangeEventArgs e)
     {
         var file = e.File;
-        if (file == null) return;
-
         if (file.ContentType != "application/json")
         {
-            _validationMessage = "invalid file type";
+            _validationMessage = "Chosen file is not JSON.";
             _positions = null;
             return;
         }
-        
-        // await using FileStream fs = new(path, FileMode.Create);
-        // await browserFile.OpenReadStream().CopyToAsync(fs);
-        
-        using var fileStream = file.OpenReadStream(); 
+
+        await using var fileStream = file.OpenReadStream(); 
         using var streamReader = new StreamReader(fileStream);
         var jsonContent = await streamReader.ReadToEndAsync();
 
@@ -51,101 +37,26 @@ public partial class JsonPositions : ComponentBase
         catch (JsonException)
         {
             _positions = null;
-            _validationMessage = "json not valid";
+            _validationMessage = "JSON not valid.";
         }
     }
-    
-    private async Task SendToBackend()
-    {
-        var response = await EmployeeService.AddPositionsJson(_positions);
-        _responseContent = await response.Content.ReadAsStringAsync();
-    }
-    
-
-    // private async Task OnDrop(
-    //     // DragEventArgs e
-    //     InputFileChangeEventArgs e)
-    // {
-    //     isDragging = false;
-    //
-    //     // if (e.DataTransfer.Files.Length > 0)
-    //     // {
-    //     //     var file = e.DataTransfer.Files[0];
-    //         try
-    //         {
-    //             var file = e.File;
-    //             
-    //             _ = JsonDocument.Parse(file.ContentType);
-    //             var stream = new MemoryStream();
-    //             await e.File.OpenReadStream().CopyToAsync(stream);
-    //             _jsonInput = stream.ToString();
-    //             
-    //             // using var streamReader = new StreamReader(file.ToString());
-    //             // _jsonInput = await streamReader.ReadToEndAsync();
-    //             
-    //             var positions = JsonSerializer.Deserialize<PositionsDto>(
-    //                 _jsonInput, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-    //             
-    //             var response = await EmployeeService.AddPositionsJson(positions!);
-    //         
-    //             if (response.StatusCode == HttpStatusCode.NoContent)
-    //             {
-    //                 _message = "Nothing to add, all positions already exist";
-    //             }
-    //             else if (response.IsSuccessStatusCode)
-    //             {
-    //                 _popupMessage = await response.Content.ReadAsStringAsync();
-    //                 await ShowSuccessPopup();
-    //                 NavigationManager.NavigateTo("/employees");
-    //             }
-    //             else
-    //             {
-    //                 _message =  "Failed to upload positions. Please check your input.";
-    //             }
-    //         }
-    //         catch (JsonException) { _message = "Please provide valid JSON file."; }
-    //         catch (Exception ex) { _message = $"An error occurred: {ex.Message}"; }
-    //     // }
-    // }
     
     private async Task UploadJson()
     {
-        if (string.IsNullOrWhiteSpace(_jsonInput)) {
-            _message = "Please provide valid JSON input.";
-            return; }
-
         try
         {
-            var positions = JsonSerializer.Deserialize<PositionsDto>(
-                _jsonInput, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-        
-            var response = await EmployeeService.AddPositionsJson(positions!);
-            
-            if (response.StatusCode == HttpStatusCode.NoContent)
-            {
-                _message = "Nothing to add, all positions already exist";
-            }
-            else if (response.IsSuccessStatusCode)
-            {
-                _popupMessage = await response.Content.ReadAsStringAsync();
-                await ShowSuccessPopup();
-                NavigationManager.NavigateTo("/employees");
-            }
-            else
-            {
-                _message =  "Failed to upload positions. Please check your input.";
-            }
+            var response = await EmployeeService.AddPositionsJson(_positions);
+
+            _responseContent = response.StatusCode == HttpStatusCode.NoContent
+                ? 
+                "Nothing to add, all positions already exist"
+                : 
+                response.IsSuccessStatusCode
+                    ? 
+                    await response.Content.ReadAsStringAsync()
+                    : 
+                    "Failed to upload positions. Please check your input.";
         }
-        catch (JsonException) { _message = "Invalid JSON format."; }
-        catch (Exception ex) { _message = $"An error occurred: {ex.Message}"; }
-    }
-    
-    private async Task ShowSuccessPopup()
-    {
-        _popupVisible = true;
-        StateHasChanged();
-        await Task.Delay(3000);
-        _popupVisible = false;
-        StateHasChanged();
+        catch (Exception e) { Console.WriteLine(e); throw; }
     }
 }
